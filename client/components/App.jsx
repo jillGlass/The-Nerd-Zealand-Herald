9e3d398a37633bc0cc09fce0019f2949e9aca616
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-11-15 12:03:02 +1300

    Update Movies component to render

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NASA from '../components/NASA'
+import Movies from './Movies'
 
 const App = () => {
   return (
     <React.Fragment>
       <h1>The Nerd-Zealand Herald</h1>
       <NASA />
+      <Movies/>
     </React.Fragment>
   )
 }
--- client/components/Movies.jsx
+++ client/components/Movies.jsx
@@ -4,55 +4,37 @@ import * as api from '../api'
 
 class Movies extends React.Component {
   state = {
-    rick:{
+    rick: {
       name: null,
       image: null
     }
   }
 
-
-  componentDidMount() {
+  componentDidMount () {
     api.getRick()
-    .then(response => {
-      this.setState({
-        rick: {
-          name: response.body.name,
-          image: response.body.image
-        }
+      .then(response => {
+        this.setState({
+          rick: {
+            name: response.body.name,
+            image: response.body.image
+          }
+        })
       })
-    })
-    
   }
-
 
   render () {
     return (
       <React.Fragment>
-        <h1></h1>
-        <img></img>
-        <p></p>
+        <h1>Rick and Morty's first episode of season 4 has been released</h1>
+        <img src = {this.state.rick.image} height = '200px'></img>
+        <p>{this.state.rick.name}</p>
+        <p>It is been a long time coming but the fourth season of Rick and Morty was released three days ago and broken Internet.</p>
+        <p>More than two years after Rick and Morty Season 3 ended, Season 4 of Adult Swim’s beloved animated sci-fi comedy kicked off November 10 — and Morty’s about to go “nuts this time.”</p>
+        <p>Rick and Morty season 4 is 10 episodes long and will unfortunately be split into two halves, so the last five episodes will arrive in 2020.</p>
+        <p>The good news is a season 5 is on its way, and this time creator Dan Harmon (the guy behind Community) seems to be running on a tighter schedule. "We're literally writing season 5 while finishing season 4 just to force ourselves to commit to a certain schedule," he said at San Diego Comic-Con.</p>
       </React.Fragment>
     )
   }
 }
-
-
-
-
-      render () {
-        return (
-          <React.Fragment>
-            <h1>Space Stuff</h1>
-            <h3>{this.state.NASA.title}</h3>
-            <p>{this.state.NASA.explanation}</p>
-            <img src = {this.state.NASA.image} height = '200px'></img>
-          </React.Fragment>
-        )
-      }
-}
-
-export default NASA
-
-
 
 export default Movies
