b9fc395d93a8268e1c179150f211eb618e4dc6b3
Jill Glass <glass.nz@gmail.com>
2019-11-15 10:49:45 +1300

    api function added for NASA

--- client/api.js
+++ client/api.js
@@ -1,2 +1,7 @@
 import request from 'superagent'
 
+const nasaURL = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY '
+
+export function getNasa () {
+  return request.get(nasaURL)
+}
--- client/components/NASA.jsx
+++ client/components/NASA.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import request from 'superagent'
+import * as api from '../api'
 
 class NASA extends React.Component {
+      state={
+        Nasa: null
+      }
 
-  componentDidMount() {
-    request.get
-  }
+      componentDidMount () {
+        api.getNasa()
+          .then(response => {
+            this.setState({
+              NASA: response.body
 
+            })
+          })
+      }
 
-  render () {
-    return (
-      <React.Fragment>
-        <h1>Incredible Earth</h1>
-        <h3>{this.state.caption}</h3>
-        <img>{this.state.url}</img>
-      </React.Fragment>
-    )
-  }
+      render () {
+        return (
+          <React.Fragment>
+            <h1>Incredible Earth</h1>
+            <h3>{this.state.caption}</h3>
+            <img>{this.state.image}</img>
+          </React.Fragment>
+        )
+      }
 }
+
+export default NASA
