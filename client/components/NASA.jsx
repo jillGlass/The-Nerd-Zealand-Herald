26fb18157d4926f80a4404b53ae806ef02958608
Jill Glass <glass.nz@gmail.com>
2019-11-15 11:30:17 +1300

    resize image to 200px

--- client/components/NASA.jsx
+++ client/components/NASA.jsx
@@ -30,7 +30,7 @@ class NASA extends React.Component {
             <h1>Space Stuff</h1>
             <h3>{this.state.NASA.title}</h3>
             <p>{this.state.NASA.explanation}</p>
-            <img src = {this.state.NASA.image}></img>
+            <img src = {this.state.NASA.image} height = '200px'></img>
           </React.Fragment>
         )
       }
